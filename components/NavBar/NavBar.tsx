b580caf29c8abbfd5156eb26659507c9c6ba9313
Roy Springer <rspringer@slip-app.nl>
2021-11-25 22:02:11 +0100

    Added navbar indicator

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import Link from "next/link";
 import Text from "../Text";
 import styles from "./NavBar.module.css";
+import { useRouter } from "next/dist/client/router";
 
 export interface NavBarItem {
   text: string;
@@ -16,15 +17,70 @@ export interface NavBarProps {
 
 const NavBar = (props: NavBarProps) => {
   const { navItems, activeIndex = 0 } = props;
+  const router = useRouter();
+  const navBar = useRef<HTMLElement>(null);
+  const indicator = useRef<HTMLDivElement>(null);
+  const indicatorHover = useRef<HTMLDivElement>(null);
+  const linkItems = useRef<(HTMLAnchorElement | null)[]>([]);
+
+  const placeIndicator = function (
+    indicatorEl: HTMLElement,
+    target: HTMLElement
+  ) {
+    const navBarRect = navBar.current?.getBoundingClientRect();
+    const elementRect = target.getBoundingClientRect();
+    indicatorEl.style.left = `calc(${
+      elementRect.x - (navBarRect?.x ?? 0) ?? 0
+    }px + 2rem)`;
+    indicatorEl.style.width = `calc(${elementRect.width * 0.9}px - 4rem)`;
+  };
+
+  const onHover = useCallback((event) => {
+    if (event.target.tagName === "A" && indicatorHover.current) {
+      placeIndicator(indicatorHover.current, event.target);
+    }
+    return false;
+  }, []);
+
+  const onLeave = useCallback((event) => {
+    if (event.target.tagName === "A") {
+      for (const item of linkItems.current) {
+        if (item?.classList.contains("active") && indicatorHover.current) {
+          placeIndicator(indicatorHover.current, item);
+        }
+      }
+    }
+    return false;
+  }, []);
+
+  useEffect(() => {
+    for (const item of linkItems.current) {
+      if (
+        item?.classList.contains("active") &&
+        indicator.current &&
+        indicatorHover.current
+      ) {
+        placeIndicator(indicator.current, item);
+        indicator.current.style.opacity = "1";
+        placeIndicator(indicatorHover.current, item);
+      }
+    }
+  }, []);
+
   return (
-    <nav className={styles.navBar}>
+    <nav ref={navBar} className={styles.navBar}>
       <ul className={styles.navBarUl}>
         {navItems.map((item, index) => (
           <li key={index} className={styles.navBarLi} onClick={item.onClick}>
             <Link href={item.navTo ?? "#"}>
               <a
+                ref={(instance) => {
+                  linkItems.current.push(instance);
+                }}
+                onMouseEnter={onHover}
+                onMouseLeave={onLeave}
                 className={`${styles.navBarA} ${
-                  index === activeIndex ? "active" : ""
+                  router.pathname === item.navTo ? "active" : ""
                 }`}
               >
                 <Text nav className={styles.navBarNumber}>
@@ -35,6 +91,8 @@ const NavBar = (props: NavBarProps) => {
             </Link>
           </li>
         ))}
+        <div ref={indicator} className={styles.indicator}></div>
+        <div ref={indicatorHover} className={styles.indicatorHover}></div>
       </ul>
     </nav>
   );
